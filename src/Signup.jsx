4a4c319753728bf4de4dda657798f5927a9b6194
= <=>
2026-01-18 22:27:53 +0500

    updated code

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+
+  const TakeEmail = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const submitForm = () => {
+    console.log(email,password);
+    
+  }
   return (
     <div>
-      <h1>Signup form</h1>
+      <div className="container">
+        <div className="form text-center">
+        <form action="">
+          <label htmlFor="">Enter Your Email</label> <br />
+          <input type="email" placeholder='Enter your email' value={email} onChange={TakeEmail}/> <br />
+          <label htmlFor="">Enter Your Password</label> <br />
+          <input type="password" placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+           
+        </form>
+
+        <button type='submit' className='btn btn-warning' onClick={submitForm}>Signup</button>
+       
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Signup
+
+
+// onChange event
